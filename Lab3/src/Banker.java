62eef6de5a91394fa7e4a86678b2aa85aeaa275f
Ross Freeman <rfreeman1@me.com>
2016-11-15 22:21:23 -0500

    Fix for mult resources

--- Lab3/src/Banker.java
+++ Lab3/src/Banker.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by rossfreeman on 11/14/16.
@@ -10,7 +11,7 @@ public class Banker extends FIFO {
         tasks = new ArrayList<>();
         processQueue = new ArrayList<>();
 
-        for(Task task : Task.tasks) {
+        for (Task task : Task.tasks) {
             tasks.add(task.duplicate());
         }
 
@@ -18,14 +19,14 @@ public class Banker extends FIFO {
     }
 
     public void run() {
-        while(hasTasks()) {
-            for(Task task : tasks) {
-                if(!processQueue.contains(task) && task.status != Status.ABORTED && task.status != Status.TERMINATED){
+        while (hasTasks()) {
+            for (Task task : tasks) {
+                if (!processQueue.contains(task) && task.status != Status.ABORTED && task.status != Status.TERMINATED) {
                     processQueue.add(task);
                 }
             }
 
-            for(Task task : processQueue) {
+            for (Task task : processQueue) {
                 if (task.status == Status.RUNNING) {
                     Activity act = task.activities.get(task.cycleNum);
 
@@ -33,18 +34,17 @@ public class Banker extends FIFO {
                         Resource resource = resources.get(act.resourceNum - 1);
                         int claim = task.resourceClaim.get(resource);
 
-                        if(task.resourceHas.containsKey(resource)) {
+                        if (task.resourceHas.containsKey(resource)) {
                             claim -= task.resourceHas.get(resource);
                         }
 
-                        if (!isSafe(task, resource, act.unitSize) ){
+                        if (!isSafe(task, resource, act.unitSize)) {
                             task.isBlocked = true;
                             task.blockCount++;
                             task.activities.add(task.cycleNum + 1, act);
-                        } else if(act.unitSize > claim) {
+                        } else if (act.unitSize > claim) {
                             abortTask(task);
-                        }
-                        else {
+                        } else {
                             resource.addResourceToTask(task, act.unitSize);
                             task.isBlocked = false;
                         }
@@ -56,22 +56,22 @@ public class Banker extends FIFO {
                     } else if (act.type == ActivityType.TERMINATE) {
                         terminateTask(task);
                         task.cycleNum++;
-                    } else if(act.type == ActivityType.INITIATE) {
-                        Resource r = resources.get(act.resourceNum-1);
-                        if(act.unitSize > r.totalUnits) {
+                    } else if (act.type == ActivityType.INITIATE) {
+                        Resource r = resources.get(act.resourceNum - 1);
+                        if (act.unitSize > r.totalUnits) {
                             abortTask(task);
                         } else {
                             task.resourceClaim.put(r, act.unitSize);
                             r.totalClaim += act.unitSize;
                             task.cycleNum++;
                         }
-                    } else if(act.type == ActivityType.COMPUTE) {
-                        if(task.computeTime == 0) {
+                    } else if (act.type == ActivityType.COMPUTE) {
+                        if (task.computeTime == 0) {
                             task.computeTime = act.resourceNum;
                         }
                         task.computeTime--;
 
-                        if(task.computeTime != 0) {
+                        if (task.computeTime != 0) {
                             task.activities.add(task.cycleNum + 1, act);
                         }
 
@@ -84,8 +84,8 @@ public class Banker extends FIFO {
 
             ArrayList<Task> toRemove = new ArrayList<>();
 
-            for(Task t : processQueue) {
-                if(!t.isBlocked || t.status != Status.RUNNING)
+            for (Task t : processQueue) {
+                if (!t.isBlocked || t.status != Status.RUNNING)
                     toRemove.add(t);
             }
 
@@ -96,7 +96,7 @@ public class Banker extends FIFO {
     }
 
     public void terminateTask(Task t) {
-        for(Resource r : t.resourceClaim.keySet()) {
+        for (Resource r : t.resourceClaim.keySet()) {
             int size = t.resourceClaim.get(r);
             r.totalClaim -= size;
         }
@@ -105,31 +105,81 @@ public class Banker extends FIFO {
     }
 
     public boolean isSafe(Task t, Resource r, int units) {
-        for(Task task : tasks) {
-            if (task.status == Status.RUNNING) {
-                int unitsNeeded = task.resourceClaim.get(r);
+        boolean [] didComplete = new boolean[tasks.size()];
+        HashMap<Resource, Integer> simResources = new HashMap<>();
 
-                if (task.resourceHas.containsKey(r)) {
-                    unitsNeeded -= task.resourceHas.get(r);
-                }
-
-                if (task == t) {
-                    unitsNeeded -= units;
-                }
-
-                if (r.unitsLeft - units >= unitsNeeded) {
-                    return true;
-                }
+        for(Resource resource : resources) {
+            if(resource == r) {
+                simResources.put(resource, resource.unitsLeft - units);
+            } else {
+                simResources.put(resource, resource.unitsLeft);
             }
         }
 
-        return false;
+        for(int i = 0; i < tasks.size(); i++) {
+            Task task = tasks.get(i);
+            if(task.status == Status.ABORTED || task.status == Status.TERMINATED) {
+                didComplete[i] = true;
+            } else {
+                didComplete[i] = false;
+            }
+        }
+
+        for(int i = 0; i < tasks.size(); i++) {
+            boolean isSafe = true;
+            Task task = tasks.get(i);
+
+            for(Resource resource : resources) {
+                if(task.resourceClaim.containsKey(resource)) {
+                    int resourcesNeeded = task.resourceClaim.get(resource);
+                    if(task.resourceHas.containsKey(resource)) {
+                        resourcesNeeded -= task.resourceHas.get(resource);
+                    }
+                    if(task == t && resource == r) {
+                        resourcesNeeded -= units;
+                    }
+                    if (resourcesNeeded > simResources.get(resource)) {
+                        isSafe = false;
+                        break;
+                    }
+                }
+            }
+
+            if(!didComplete[i] && isSafe) {
+                for(Resource resource : resources) {
+                    int current = simResources.get(resource);
+
+                    int toAdd = 0;
+                    if(task.resourceHas.containsKey(resource)) {
+                        toAdd += task.resourceHas.get(resource);
+                    }
+
+                    if(task == t && resource == r) {
+                        toAdd += units;
+                    }
+
+                    current += toAdd;
+
+                    simResources.replace(resource, current);
+                }
+
+                didComplete[i] = true;
+                i--;
+            }
+        }
+
+        for(int i = 0; i < didComplete.length; i++) {
+            if(!didComplete[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public void abortTask(Task t) {
         t.status = Status.ABORTED;
 
-        for(Resource r : t.resourceHas.keySet()) {
+        for (Resource r : t.resourceHas.keySet()) {
             r.removeResourceFromTask(t, -1);
         }
 
@@ -142,17 +192,17 @@ public class Banker extends FIFO {
         int waitSum = 0;
 
         System.out.println("\t\tBanker's");
-        for(int i = 0; i < tasks.size(); i++) {
-            System.out.print("Task " + (i+1) + "\t\t");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.print("Task " + (i + 1) + "\t\t");
             Task t = tasks.get(i);
             t.printDetails();
 
-            if(t.status != Status.ABORTED) {
+            if (t.status != Status.ABORTED) {
                 cycleSum += tasks.get(i).cycleNum;
                 waitSum += t.blockCount;
             }
         }
-        double pct = (double)waitSum/cycleSum * 100;
+        double pct = (double) waitSum / cycleSum * 100;
         System.out.println("total \t\t" + cycleSum + "\t" + waitSum + "\t " + pct + "%");
     }
 }
